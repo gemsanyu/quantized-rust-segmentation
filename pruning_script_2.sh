454a1ea1f1df02837da73cebf9471fc002a2d600
gemilang <git.gemilang.santiyuda@gmail.com>
2024-05-28 00:50:14 +0800

    otw pruning training

--- pruning_script_2.sh
+++ pruning_script_2.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-python pruning.py --arch fpn --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch pan --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch deeplabv3 --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch unet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch manet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch linknet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch pspnet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch unet++ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-python pruning.py --arch deeplabv3+ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.8
-
-
-python pruning.py --arch fpn --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch pan --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch deeplabv3 --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch unet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch manet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch linknet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch pspnet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch unet++ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
-python pruning.py --arch deeplabv3+ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.9
+python pruning.py --arch fpn --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch pan --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch deeplabv3 --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch unet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch manet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch linknet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch pspnet --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch unet++ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
+python pruning.py --arch deeplabv3+ --dataset NEA --batch-size 4 --lr 3e-4 --max-epoch 500 --device cuda --encoder mobilenet_v2 --title exp_1 --sparsity 0.5
